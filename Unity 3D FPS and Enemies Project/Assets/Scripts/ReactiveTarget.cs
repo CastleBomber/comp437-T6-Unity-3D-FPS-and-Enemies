c0c021f742269ac46a31fba86e5a00cb74b84de6
CastleBomber <richie.ucsb@gmail.com>
2020-10-17 14:20:39 -0700

    enemyPrefab

---
+++ Unity 3D FPS and Enemies Project/Assets/Scripts/ReactiveTarget.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0514913dae5b21004e281918536a97ced4afa09dd44cddf54e6e377fc238f012
+size 439
---
+++ Unity 3D FPS and Enemies Project/Assets/WanderingAI.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d92259af431aca4ea9d6de2c14118e45bdee847a8f1cd5f12787100c80a26da3
+size 752
